7e8bf7fd1fc93334061ed613b3fd8ade590e3519
FreeSlave <freeslave93@gmail.com>
2015-12-05 21:49:13 +0300

    Add license

--- qspr/spritedefs.h
+++ qspr/spritedefs.h
@@ -1,3 +1,11 @@
+/* Copyright (C) 2015 Roman Chistokhodov <freeslave93@gmail.com>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License (LGPL) as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+*/
+
 #ifndef SPRITEDEFS_H
 #define SPRITEDEFS_H
 
